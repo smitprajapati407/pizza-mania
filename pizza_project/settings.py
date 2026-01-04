48a7d8c5b06c55b8af99574141fd7d6c35c0b98a
smitprajapati407 <prajapatismit407@gmail.com>
2026-01-04 19:46:14 +0530

    Add dj-database-url and psycopg2 for PostgreSQL

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -32,6 +32,7 @@ SECURE_SSL_REDIRECT = True
 
 import os
 import cloudinary
+import dj_database_url
 # Application definition
 
 INSTALLED_APPS = [
@@ -83,10 +84,11 @@ WSGI_APPLICATION = 'pizza_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
