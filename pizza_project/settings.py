ea45c84d8914ee3adc951667e39a0b569527f85c
smitprajapati407 <prajapatismit407@gmail.com>
2026-01-04 13:11:48 +0530

    Fix Cloudinary HTTPS and make site responsive for mobile

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -142,4 +142,5 @@ cloudinary.config(
     cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
     api_key=os.environ.get("CLOUDINARY_API_KEY"),
     api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+    secure=True
 )
