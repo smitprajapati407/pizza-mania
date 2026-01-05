84ba7fc84de3a23f203b2c9276ff0c3244321b32
smitprajapati407 <prajapatismit407@gmail.com>
2026-01-05 09:35:47 +0530

    Fix deployment configuration and cleanup

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8^3o3(mt1s=5f_lctfw2i$7f-b1pm%kmf(9v0a-!aradg24cdf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
