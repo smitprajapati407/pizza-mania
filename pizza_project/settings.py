fe3808dd8e39fa73de4d10757205d47aca7905c3
smitprajapati407 <prajapatismit407@gmail.com>
2026-01-05 09:48:03 +0530

    Fix database configuration for PostgreSQL using DATABASE_URL

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -84,13 +84,12 @@ WSGI_APPLICATION = 'pizza_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
