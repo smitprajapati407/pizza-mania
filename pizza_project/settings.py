c7e4f9ece784ec746ddb61b97370edab3abe3dc3
smitprajapati407 <prajapatismit407@gmail.com>
2026-01-08 20:31:50 +0530

    Remove temporary admin auto-create code

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
-    'store.apps.StoreConfig',
+    'store',
     'orders',
     'accounts',
     
--- store/apps.py
+++ store/apps.py
@@ -3,17 +3,3 @@ from django.apps import AppConfig
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
-
-    def ready(self):
-        from django.contrib.auth.models import User
-        from django.db.utils import OperationalError
-
-        try:
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    username="admin",
-                    email="admin@example.com",
-                    password="Admin@12345"
-                )
-        except OperationalError:
-            pass
